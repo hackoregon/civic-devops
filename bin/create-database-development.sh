a0258fc9442ae227eed70795fe69835c06913015
Mike Lonergan <mikethecanuck@gmail.com>
2018-02-25 10:15:43 -0800

    DATA_DIRECTORY variable for cleaner code (and easier testing)

--- bin/create-database-development.sh
+++ bin/create-database-development.sh
@@ -6,11 +6,15 @@ sudo yum update -y
 sudo amazon-linux-extras install postgresql9.6 # enables postgres9.6 install on Amazon Linux 2
 sudo yum install postgresql.x86_64 postgresql-server.x86_64 # aliases to postgresql 9.6.6-1.amzn2.0.1 as of 2018-02-25
 
-mkdir /data/databases # Create data_directory
-sudo chown postgres /data/databases
+DATA_DIRECTORY="/data/databases"
+
+# Assumes secondary volume is mounted as /data
+
+mkdir $DATA_DIRECTORY
+sudo chown postgres $DATA_DIRECTORY
 
 # sudo service postgresql96 initdb --pgdata=/data/databases # I believe this worked on Amazon Linux (not AL "2")
-sudo -u postgres initdb --pgdata=/data/databases
+sudo -u postgres initdb --pgdata=$DATA_DIRECTORY
 sudo service postgresql96 start # errors out - 'Directory "/var/lib/pgsql/data" is missing or empty.''
 
 # Note: this should prompt for console input to set the 'postgres' OS account password

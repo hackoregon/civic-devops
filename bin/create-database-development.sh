b1d3923c93634b0eab680011dfc805535faaa900
Mike Lonergan <mikethecanuck@gmail.com>
2018-02-25 09:53:25 -0800

    Modified for use on Amazon Linux 2, and as flaws in the proposed commands shake out in testing

--- bin/create-database-development.sh
+++ bin/create-database-development.sh
@@ -3,10 +3,16 @@
 # TODO: declare the yum package as a variable up front for visibility
 
 sudo yum update -y
-sudo yum install postgresql96.x86_64
+sudo amazon-linux-extras install postgresql9.6 # enables postgres9.6 install on Amazon Linux 2
+sudo yum install postgresql.x86_64 postgresql-server.x86_64 # aliases to postgresql 9.6.6-1.amzn2.0.1 as of 2018-02-25
+
 mkdir /data/databases # Create data_directory
-sudo service postgresql96 initdb --pgdata=/data/databases
-sudo service postgresql96 start
+sudo chown postgres /data/databases
+
+# sudo service postgresql96 initdb --pgdata=/data/databases # I believe this worked on Amazon Linux (not AL "2")
+sudo -u postgres initdb --pgdata=/data/databases
+sudo service postgresql96 start # errors out - 'Directory "/var/lib/pgsql/data" is missing or empty.''
+
 # Note: this should prompt for console input to set the 'postgres' OS account password
 # <<EXPECT CONSOLE INPUT>>
 sudo -u postgres passwd

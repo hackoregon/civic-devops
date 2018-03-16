70c2006bc83fdf8f0e29126fb951fd23c2af2ffe
Khuong Luu <6059347+khashf@users.noreply.github.com>
2018-03-15 20:20:07 -0700

    Improve launch script: output the new instance's public ip after launch

--- bin/create-ec2-machine-database.sh
+++ bin/create-ec2-machine-database.sh
@@ -1,14 +1,26 @@
 #!/bin/bash -e
 
-# if [ "$#" -ne 1 ]; then
-#     echo "Must has 1 argument as instance name"
-#     exit 1
-# fi
+if [ "$#" -ne 1 ]; then
+    echo "Must has 1 argument as instance tag name"
+    exit 1
+fi
 
+# Source ec2 specs from a separate file
+# The ./ec2-profile.sh should contain the following variables
+# DEVICENAME='/dev/sdb'
+# IMAGEID='ami-7f43f307'
+# INSTANCETYPE='t2.micro'
+# KEYNAME='hackoregon-2018-database-dev-env'
+# REGION='us-west-2'
+# SECURITYGROUPIDS='sg-28154957'
+# SUBNETID='subnet-8794fddf'
+# VOLUMESIZE='8'
+source ./my-ec2-profile.sh
 
 instance_name=$1
 tag_specs='ResourceType=instance,Tags=[{Key=Name,Value='$instance_name'}]'
-echo $tag_specs
+
+echo "Launching the ec2 "\"$instance_name\"" instance..."
 aws ec2 run-instances \
    --image-id $IMAGEID \
    --count 1 \
@@ -19,4 +31,4 @@ aws ec2 run-instances \
    --region $REGION \
    --block-device-mappings "[{\"DeviceName\":\"/dev/sdb\",\"Ebs\":{\"VolumeSize\":8,\"DeleteOnTermination\":true}}]" \
    --tag-specifications $tag_specs \
-   --query 'Instances[0].InstanceId'
+   --query 'Instances[0].InstanceId' \

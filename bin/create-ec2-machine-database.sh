01dbb2e862bce4e928d091b0c2435ca639d0b892
Khuong Luu <6059347+khashf@users.noreply.github.com>
2018-03-15 20:17:52 -0700

    Improve ec2 launch script: Load desire ec2 profile from a separate file

--- bin/create-ec2-machine-database.sh
+++ bin/create-ec2-machine-database.sh
@@ -5,14 +5,6 @@
 #     exit 1
 # fi
 
-DEVICENAME='/dev/sdb'
-IMAGEID='ami-7f43f307'
-INSTANCETYPE='t2.micro'
-KEYNAME='hackoregon-2018-database-dev-env'
-REGION='us-west-2'
-SECURITYGROUPIDS='sg-28154957'
-SUBNETID='subnet-8794fddf'
-VOLUMESIZE='8'
 
 instance_name=$1
 tag_specs='ResourceType=instance,Tags=[{Key=Name,Value='$instance_name'}]'

b8e70829801b2e7dfad7f41e33bc264feab9f289
Khuong Luu <6059347+khashf@users.noreply.github.com>
2018-03-18 15:39:46 -0700

    Improve comments

--- bin/restore-database.sh
+++ bin/restore-database.sh
@@ -1,35 +1,47 @@
 #! /bin/sh
 
-# Restore a single database instance from a shell session running on the PostgreSQL server
+# Intentions:
+# - Restore a single database instance from a shell session running on the PostgreSQL server
+
 # Usage: 
-# (1) Feed with parameters
+# (1) Feed with parameters: 
+#     $ restore-database.sh <database_instance_name> <database_user_name> <database_user_password>
 # or
-# (2) Use interatively (no parameters)
+# (2) Use interatively (no parameters): 
+#     $ restore-database.sh
 
 # Result:
-# - The database is successfully restored to the target instance
+# - The database is successfully restored to the target
 
 # Prerequisites:
 # - Postgres has been configured and started
 # - Admin-level permissions to the database
 # - Database instance is configured with Owner access for the user context
+# - awscli tools installed to access s3:// paths
+
+# TODO: figure out how to authenticate from EC2 box to S3 bucket
+
+# Install AWSCLI
+
 
 if [ "$#" -eq 5 ]; then
 	# Running with 5 arguments
 	DATABASE_NAME=$1
 	USERNAME=$2
 	PASSWORD=$3
-    DATABASE_SERVER=$4
-    BACKUP_FILE=$5
+    DATABASE_SERVER=$4 # e.g. ec2-34-214-158-132.us-west-2.compute.amazonaws.com
+    BACKUP_FILE=$5 # e.g. 
 
+    # TODO: clean up this branch
     sudo -u postgres pg_restore \
          --host $DATABASE_SERVER \
          --port "5432" \
          --username $USERNAME \
          --no-password \
          --dbname $DATABASE_NAME \
-         --verbose \
-         $BACKUP_FILE
+         --verbose 
+         $BACKUP_FILE # eg. s3://bucket-name/folder/filename.backup
+                      #     or https://s3-us-west-2.amazonaws.com/hacko-data-archvie/passenger_census.backup
 
 elif [ "$#" -eq 0 ]; then
 	# Running interatively (no arguments)

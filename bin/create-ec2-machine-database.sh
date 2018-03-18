88e73b133ef5830c3bc18b80ef4ddfc02b22a8ab
Khuong Luu <6059347+khashf@users.noreply.github.com>
2018-03-18 14:39:03 -0700

    Separate improvement on instance creation script to another branch

--- bin/create-ec2-machine-database.sh
+++ bin/create-ec2-machine-database.sh
@@ -5,19 +5,14 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-# Source ec2 specs from a separate file
-# The ./ec2-profile.sh should contain the following variables
-# DEVICENAME='/dev/sdb'
-# IMAGEID='ami-7f43f307'
-# INSTANCETYPE='t2.micro'
-# KEYNAME='hackoregon-2018-database-dev-env'
-# REGION='us-west-2'
-# SECURITYGROUPIDS='sg-28154957'
-# SUBNETID='subnet-8794fddf'
-# VOLUMESIZE='8'
-
-EC2PROFILE='./ec2-profile.sh'
-source $EC2PROFILE
+DEVICENAME='/dev/sdb'
+IMAGEID='ami-7f43f307'
+INSTANCETYPE='t2.micro'
+KEYNAME='hackoregon-2018-database-dev-env'
+REGION='us-west-2'
+SECURITYGROUPIDS='sg-28154957'
+SUBNETID='subnet-8794fddf'
+VOLUMESIZE='8'
 
 instance_name=$1
 tag_specs='ResourceType=instance,Tags=[{Key=Name,Value='$instance_name'}]'

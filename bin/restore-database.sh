e14c4e7e4d47915056420ca651f5ef14bbcbbbc2
Khuong Luu <6059347+khashf@users.noreply.github.com>
2018-03-18 22:39:42 -0700

    Complete: Download s3 file to local and restore to db

--- bin/restore-database.sh
+++ bin/restore-database.sh
@@ -17,54 +17,61 @@
 # - Postgres has been configured and started
 # - Admin-level permissions to the database
 # - Database instance is configured with Owner access for the user context
-# - awscli tools installed to access s3:// paths
+# - The file located at $S3_BACKUP_FILE has to be a pg_dump in binary compatible
+#   with the currently used Postgres version
+# - This ec2 instance has permission to read from s3. 
+#   To has such permission, this ec2 instance must have been launched with 
+#   the flag `--iam-instance-profile Name=$INSTANCE_PROFILE_NAME `, where
+#   the $INSTANCE_PROFILE_NAME is the instance profile that was once created 
+#   by running `create-instance-profile.sh`
 
-# TODO: figure out how to authenticate from EC2 box to S3 bucket
+DATABASE_NAME=
+USERNAME=
+PASSWORD=
+DATABASE_SERVER= # e.g. ec2-34-214-158-132.us-west-2.compute.amazonaws.com
+LOCAL_BACKUP_FILE= # e.g. './passenger_census.backup'
+S3_BACKUP_FILE= # e.g. 's3://luukh/test1/passenger_census.backup'
 
-# Install AWSCLI
-
-
+# Get parameters from user
 if [ "$#" -eq 5 ]; then
 	# Running with 5 arguments
 	DATABASE_NAME=$1
 	USERNAME=$2
 	PASSWORD=$3
-    DATABASE_SERVER=$4 # e.g. ec2-34-214-158-132.us-west-2.compute.amazonaws.com
-    BACKUP_FILE=$5 # e.g. 
-
-    # TODO: clean up this branch
-    sudo -u postgres pg_restore \
-         --host $DATABASE_SERVER \
-         --port "5432" \
-         --username $USERNAME \
-         --no-password \
-         --dbname $DATABASE_NAME \
-         --verbose 
-         $BACKUP_FILE # eg. s3://bucket-name/folder/filename.backup
-                      #     or https://s3-us-west-2.amazonaws.com/hacko-data-archvie/passenger_census.backup
-
+    DATABASE_SERVER=$4 
+    S3_BACKUP_FILE=$5 
 elif [ "$#" -eq 0 ]; then
 	# Running interatively (no arguments)
     read -p "Database server name: " DATABASE_SERVER
 	read -p "Database instance name: " DATABASE_NAME
 	read -p "Username: " USERNAME
-    read -p "backup file path and name: " BACKUP_FILE
-	echo "Restoring backup file to $DATABASE_NAME - prompts for password..."
-#	sudo -u postgres createuser --encrypted --pwprompt --no-createdb --no-createrole --no-superuser --no-replication $USERNAME
-    sudo -u postgres pg_restore \
-         --host $DATABASE_SERVER \
-         --port "5432" \
-         --username $USERNAME \
-         --no-password \
-         --dbname $DATABASE_NAME \
-         --verbose \
-         $BACKUP_FILE
-
+    read -p "backup file path and name: " S3_BACKUP_FILE
 else
-    # TODO: clean up this branch
-	echo "Error: Must be 0 or 3 parameters"
+	echo "Error: Must be 0 or 5 parameters"
 	echo 'Usage:'
 	echo '(1) Feed with parameters: sudo create-db.sh <database_instance_name> <database_user_name> <database_user_password>'
 	echo 'or (2) Use interatively (no parameters): sudo create-db.sh'
 	exit 1
 fi
+
+echo "Downloading $S3_BACKUP_FILE to $LOCAL_BACKUP_FILE"
+aws s3 cp $S3_BACKUP_FILE $LOCAL_BACKUP_FILE
+if [ $? != 0 ]; then
+    echo 'Downloading failed'
+    exit 1
+done
+echo
+
+echo 'Downloaded file size: '
+ls -l --block-size=M $LOCAL_BACKUP_FILE
+
+echo "Restoring backup file to $DATABASE_NAME..."
+#sudo -u postgres createuser --encrypted --pwprompt --no-createdb --no-createrole --no-superuser --no-replication $USERNAME
+sudo -u postgres pg_restore \
+    --host $DATABASE_SERVER \
+    --port "5432" \
+    --username $USERNAME \
+    --no-password \
+    --dbname $DATABASE_NAME \
+    --verbose \
+    $LOCAL_BACKUP_FILE

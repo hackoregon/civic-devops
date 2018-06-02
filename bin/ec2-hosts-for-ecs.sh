fad17d67c5c927461a3c26a3a751ffcaf2126108
Mike Lonergan <mikethecanuck@gmail.com>
2018-06-02 12:38:37 -0700

    Convenience script for use on our bastion host, to be able to reach our EC2 hosts without having to look up their IP address every single time

---
+++ bin/ec2-hosts-for-ecs.sh
@@ -0,0 +1,5 @@
+# run the source command on this file to simplify your ssh commands from bastion host to the EC2 hosts
+# e.g. ssh -i pem_file.pem $ECS_HOST_USWEST_2A
+
+export ECS_HOST_USWEST_2A=10.180.24.96
+export ECS_HOST_USWEST_2B=10.180.35.77

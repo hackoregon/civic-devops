f857c2ea8215c9eb74170a9adec22be46d618331
Mike Lonergan <mikethecanuck@gmail.com>
2018-06-02 19:47:05 -0700

    updated to current data volume size as of right now

---
+++ bin/ec2-profile-db-testing.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e 
+
+DEVICENAME='/dev/sdb'
+DELETEONTERM='true'
+IMAGEID='ami-07eb707f'
+INSTANCETYPE='t2.medium'
+KEYNAME='hackoregon-2018-database-dev-env'
+REGION='us-west-2'
+SECURITYGROUPIDS='sg-28154957'
+SUBNETID='subnet-8794fddf'
+VOLUMESIZE='20'
+VOLUMETYPE='gp2'
+
